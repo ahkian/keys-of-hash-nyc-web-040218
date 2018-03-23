f45b6dfa486d52b8a24ca4babf77fd7c848d3421
Daniel Brazel <dbrazel4@gmail.com>
2018-03-23 17:04:54 +0000

    automatically backed up by learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,6 +1,11 @@
 class Hash
   def keys_of(*arguments)
     returning_arr = []
-
+    self.each do |k, v|
+      if v == arguments
+        returning_arr << k
+      end
+    end
+    returning_arr
   end
 end
